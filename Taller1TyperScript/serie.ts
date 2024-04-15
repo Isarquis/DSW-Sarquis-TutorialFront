28f343bdb6a900634146dc7e47f884a502ec0afc
Isabella <i.sarquis@uniandes.edu.co>
2024-04-14 22:48:55 -0500

    Taller2 Fianl

--- Taller1TyperScript/serie.ts
+++ Taller1TyperScript/serie.ts
@@ -1,6 +1,6 @@
 
 export class Serie{
-    constructor(public id:number, public nombre:String, public channel:String, public seasons:number, public plot:String, public cite:String,public image:String)
+    constructor(public id:number, public nombre:string, public channel:String, public seasons:number, public plot:String, public cite:String,public image:String)
     {
 
     }
